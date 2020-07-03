7884d49e139d054f29b327747ad6885c73d852e0
James Pyle <jamesmpyle@gmail.com>
2020-07-03 07:26:50 -0400

    update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,7 +1,7 @@
 import os
 import csv
 
-csvpath = os.path.join('PyPoll', 'Resources','election_data.csv')
+csvpath = os.path.join('Resources','election_data.csv')
 
 data = csv.DictReader(open(csvpath))
 candidates = []
