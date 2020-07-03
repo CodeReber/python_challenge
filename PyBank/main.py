8a10ecca13cdb7f40bfff4ccf689dac1c57cc271
James Pyle <jamesmpyle@gmail.com>
2020-07-03 07:14:04 -0400

    update

---
+++ PyBank/main.py
@@ -0,0 +1,38 @@
+import os
+import csv
+import math
+
+csvpath = os.path.join('Resources', 'budget_data.csv')
+
+
+input_file = csv.DictReader(open(csvpath))
+
+total_line = 0
+total_amount = 0
+total_avr = 0
+max_num = 0
+low_num = 0
+current_date = None
+current_date2 = None  
+for row in input_file:
+    num = int(row['Profit/Losses'])
+    date = str(row['Date'])
+    total_line += 1
+    total_amount += num  
+    if max_num == max_num < num:
+        max_num = num
+        current_date = date
+    if low_num == low_num > num:
+        low_num = num
+        current_date2 = date
+
+total_avr = total_line/total_amount
+
+        
+print('\nFinancial Analysis')
+print('------------------------------')
+print(f'\nTotal Months:{total_line}')
+print(f'Total: ${total_amount}')
+print(f'Average Change: ${total_avr}')
+print(f'Greatest Increase in Profits: {current_date} (${max_num})')
+print(f'Greatest Decrease in Profits: {current_date2} (${low_num})')
